fe9299eb957e561ea5045c5d83c3fadb50c07be2
骆越 <guhongliang.ghl@antgroup.com>
2023-03-22 22:56:46 +0800

    fit to my own use

--- src/commands/repo/browse.rs
+++ src/commands/repo/browse.rs
@@ -3,31 +3,34 @@ use crate::finder::structures::{Opts as FinderOpts, SuggestionType};
 
 use crate::common::git;
 use crate::prelude::*;
-use std::fs;
+use std::{env, fs};
 
 pub fn main() -> Result<String> {
     let finder = CONFIG.finder();
 
-    let repo_pathbuf = {
-        let mut p = filesystem::tmp_pathbuf()?;
-        p.push("featured");
-        p
-    };
+    // let repo_pathbuf = {
+    //     let mut p = filesystem::tmp_pathbuf()?;
+    //     p.push("featured");
+    //     p
+    // };
 
-    let repo_path_str = &repo_pathbuf.to_string();
+    // let repo_path_str = &repo_pathbuf.to_string();
 
-    let _ = filesystem::remove_dir(&repo_pathbuf);
-    filesystem::create_dir(&repo_pathbuf)?;
+    // let _ = filesystem::remove_dir(&repo_pathbuf);
+    // filesystem::create_dir(&repo_pathbuf)?;
 
-    let (repo_url, _, _) = git::meta("denisidoro/cheats");
-    git::shallow_clone(repo_url.as_str(), repo_path_str)
-        .with_context(|| format!("Failed to clone `{}`", repo_url))?;
+    // let (repo_url, _, _) = git::meta("denisidoro/cheats");
+    // git::shallow_clone(repo_url.as_str(), repo_path_str)
+    //     .with_context(|| format!("Failed to clone `{}`", repo_url))?;
 
-    let feature_repos_file = {
-        let mut p = repo_pathbuf.clone();
-        p.push("featured_repos.txt");
-        p
-    };
+    // let feature_repos_file = {
+    //     let mut p = repo_pathbuf.clone();
+    //     p.push("featured_repos.txt");
+    //     p
+    // };
+    // let feature_repos_file = "$HOME/etc/navi/featured_repos.txt";
+    let home = env::var("HOME").unwrap();
+    let feature_repos_file = format!("{}/etc/navi/featured_repos.txt", home);
 
     let repos = fs::read_to_string(&feature_repos_file).context("Unable to fetch featured repositories")?;
 
@@ -46,7 +49,7 @@ pub fn main() -> Result<String> {
         })
         .context("Failed to get repo URL from finder")?;
 
-    filesystem::remove_dir(&repo_pathbuf)?;
+    // filesystem::remove_dir(&repo_pathbuf)?;
 
     Ok(repo)
 }
